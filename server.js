7af0978a6ae1cb393d4aa5696bf13f9dd3e7987e
Manuel Kipfer <manuel.kipfer@swisscom.com>
2017-08-11 14:04:57 +0200

    Add handlers for playlists Add passport

--- server.js
+++ server.js
@@ -5,33 +5,51 @@ const RadioStream = require('ultritium-radio-stream');
 const express = Express();
 const passport = require('passport');
 const BearerStrategy = require('passport-http-bearer').Strategy;
+const mongoose   = require('mongoose');
+const _ = require('lodash');
+const cors = require('cors');
+
 const StreamService = require('./lib/service/stream/stream');
 const PlaylistService = require('./lib/service/playlist/playlist');
 const PlaylistItemService = require('./lib/service/playlistitem/playlistitem');
-const mongoose   = require('mongoose');
+const AuthService = require('./lib/service/auth/auth');
+
+const AuthenticateMiddleware = require('./lib/service/auth/middleware/authenticate');
+
 const User = require('./lib/model/user');
 
 mongoose.connect('mongodb://localhost:27017/radiostation');
 
+
+passport.use(new BearerStrategy((token, next) => {
+  User.findOne({ token: token }, (err, result) => {
+    console.log(result);
+    if (err) return next(err);
+    if (!result) return next (new Error('No valid token'));
+
+    return next(result, {});
+  });
+}));
+
 express.use(cookieParser());
 express.use(bodyParser.urlencoded({ extended: true }));
 express.use(bodyParser.json());
+express.use(cors());
 express.use(passport.initialize());
 
-passport.use(new BearerStrategy((token, next) => {
-
-  console.log('ughblahh');
-  return done(new Error());
-}));
-
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8090;
 
 const router = Express.Router();
 
-express.use(passport.authenticate('bearer', { session: false }));
+//express.use(passport.authenticate('bearer', { session: false }));
+//express.use(AuthenticateMiddleware.handle());
 
-router.get('/', (req, res) => {
-    res.json({ message: 'hooray! welcome to our api!' });
+router.post('/auth', (req, res, next) => {
+  AuthService.handle('POST', req, res, next);
+});
+
+router.delete('/auth', (req, res, next) => {
+  AuthService.handle('DELETE', req, res, next);
 });
 
 router.get('/stream/:type/:playlistId', (req, res, next) => {
